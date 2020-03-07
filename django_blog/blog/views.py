71003840ae31bceeaa547f33476805ac21f64517
Jakub <kontakt@jakub0301.net>
2020-03-07 10:44:52 +0100

    Added post model

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
 
-posts = [
-    {
-        'author': 'Jakub',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'March 5, 2020'
-
-    },
-    {
-        'author': 'Jane',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 6, 2020'
-
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
 
